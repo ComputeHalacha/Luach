76fa25cb10957451ffc98db0c1e70fafe03b3c93
Chaim B. Sommers <cb@compute.co.il>
2017-10-23 16:55:31 +0300

    Corrected baha"b dates.

--- App/Code/JCal/jDate.js
+++ App/Code/JCal/jDate.js
@@ -926,13 +926,13 @@ export default class jDate {
                     list.push(!hebrew ? 'Last Day of Pesach' : 'אחרון של פסח');
                 break;
             case 2: //Iyar
-                if (dayOfWeek === 1 && jDay > 2 && jDay < 12) {
+                if (dayOfWeek === 1 && jDay > 3 && jDay < 13) {
                     list.push(!hebrew ? 'Bahab' : 'תענית שני קמא');
                 }
-                else if (dayOfWeek === 4 && jDay > 5 && jDay < 13) {
+                else if (dayOfWeek === 4 && jDay > 6 && jDay < 14) {
                     list.push(!hebrew ? 'Bahab' : 'תענית חמישי');
                 }
-                else if (dayOfWeek === 1 && jDay > 9 && jDay < 17) {
+                else if (dayOfWeek === 1 && jDay > 10 && jDay < 18) {
                     list.push(!hebrew ? 'Bahab' : 'תענית שני בתרא');
                 }
                 if (jDay === 14)
@@ -1005,13 +1005,13 @@ export default class jDate {
                     list.push(!hebrew ? 'Simchas Torah' : 'שמחת תורה');
                 break;
             case 8: //Cheshvan
-                if (dayOfWeek === 1 && jDay > 2 && jDay < 12) {
+                if (dayOfWeek === 1 && jDay > 3 && jDay < 13) {
                     list.push(!hebrew ? 'Bahab' : 'תענית שני קמא');
                 }
-                else if (dayOfWeek === 4 && jDay > 5 && jDay < 13) {
+                else if (dayOfWeek === 4 && jDay > 6 && jDay < 14) {
                     list.push(!hebrew ? 'Bahab' : 'תענית חמישי');
                 }
-                else if (dayOfWeek === 1 && jDay > 9 && jDay < 17) {
+                else if (dayOfWeek === 1 && jDay > 10 && jDay < 18) {
                     list.push(!hebrew ? 'Bahab' : 'תענית שני בתרא');
                 }
                 if (jDay === 7 && israel)
