08065fdc35caa959aa0b2f02a4ccbfd1dc2da2d2
Chaim B. Sommers <cb@compute.co.il>
2019-05-01 23:13:02 +0300

    Version 1.65: Changed default location to Lakewood by using a "firts-time" trick. Removed location from main screen. Made Tahara events in daily component darker.

--- App/Code/Data/AppData.js
+++ App/Code/Data/AppData.js
@@ -1,34 +1,88 @@
+import firstTime from 'react-native-catch-first-time';
 import DataUtils from './DataUtils';
 import Settings from '../Settings';
+import Location from '../JCal/Location';
 import Entry from '../Chashavshavon/Entry';
 import { Kavuah } from '../Chashavshavon/Kavuah';
 import EntryList from '../Chashavshavon/EntryList';
 import { error, warn } from '../GeneralUtils';
-
 /**
  * List of fields that have been added after the initial app launch.
  * Any that do not yet exist, will be added to the db schema during initial loading.
  */
 const addedFields = [
-    //Added 5/10/17
-    { table: 'settings', name: 'keepThirtyOne', type: 'BOOLEAN', allowNull: false, defaultValue: '1' },
-    //Added 5/28/17
-    { table: 'settings', name: 'showIgnoredKavuahs', type: 'BOOLEAN', allowNull: true },
-    //Added 6/1/17
-    { table: 'entries', name: 'ignoreForKavuah', type: 'BOOLEAN', allowNull: true },
-    { table: 'entries', name: 'ignoreForFlaggedDates', type: 'BOOLEAN', allowNull: true },
-    { table: 'entries', name: 'comments', type: 'VARCHAR (500)', allowNull: true },
-    //Added 6/27/17
-    { table: 'settings', name: 'haflagaOfOnahs', type: 'BOOLEAN', allowNull: true },
-    //Added 6/28/17
-    { table: 'settings', name: 'noProbsAfterEntry', type: 'BOOLEAN', allowNull: true, defaultValue: '1' },
-    //Added 6/29/17
-    { table: 'settings', name: 'kavuahDiffOnahs', type: 'BOOLEAN', allowNull: true },
-    //Added 7/3/17
-    { table: 'settings', name: 'hideHelp', type: 'BOOLEAN', allowNull: true },
-    //Added 8/8/17
-    { table: 'occasions', name: 'color', type: 'VARCHAR (25)', allowNull: true },
-];
+        //Added 5/10/17
+        {
+            table: 'settings',
+            name: 'keepThirtyOne',
+            type: 'BOOLEAN',
+            allowNull: false,
+            defaultValue: '1',
+        },
+        //Added 5/28/17
+        {
+            table: 'settings',
+            name: 'showIgnoredKavuahs',
+            type: 'BOOLEAN',
+            allowNull: true,
+        },
+        //Added 6/1/17
+        {
+            table: 'entries',
+            name: 'ignoreForKavuah',
+            type: 'BOOLEAN',
+            allowNull: true,
+        },
+        {
+            table: 'entries',
+            name: 'ignoreForFlaggedDates',
+            type: 'BOOLEAN',
+            allowNull: true,
+        },
+        {
+            table: 'entries',
+            name: 'comments',
+            type: 'VARCHAR (500)',
+            allowNull: true,
+        },
+        //Added 6/27/17
+        {
+            table: 'settings',
+            name: 'haflagaOfOnahs',
+            type: 'BOOLEAN',
+            allowNull: true,
+        },
+        //Added 6/28/17
+        {
+            table: 'settings',
+            name: 'noProbsAfterEntry',
+            type: 'BOOLEAN',
+            allowNull: true,
+            defaultValue: '1',
+        },
+        //Added 6/29/17
+        {
+            table: 'settings',
+            name: 'kavuahDiffOnahs',
+            type: 'BOOLEAN',
+            allowNull: true,
+        },
+        //Added 7/3/17
+        {
+            table: 'settings',
+            name: 'hideHelp',
+            type: 'BOOLEAN',
+            allowNull: true,
+        },
+        //Added 8/8/17
+        {
+            table: 'occasions',
+            name: 'color',
+            type: 'VARCHAR (25)',
+            allowNull: true,
+        },
+    ],
+    GLOBAL_FIRST_TIME_RANDOM = 'ed92c2efd74740dbb2da04f17ff922b1';
 
 /**
  * An single object that contains all the application data.
@@ -43,7 +97,14 @@ export default class AppData {
      * @param {[ProblemOnah]} problemOnahs
      * @param {[TaharaEvent]} taharaEvents
      */
-    constructor(settings, occasions, entryList, kavuahList, problemOnahs, taharaEvents) {
+    constructor(
+        settings,
+        occasions,
+        entryList,
+        kavuahList,
+        problemOnahs,
+        taharaEvents
+    ) {
         this.Settings = settings || new Settings({});
         this.UserOccasions = occasions || [];
         this.EntryList = entryList || new EntryList();
@@ -58,7 +119,10 @@ export default class AppData {
         this.EntryList.calulateHaflagas();
         let probs = [];
         if (this.EntryList.list.length > 0) {
-            probs = this.EntryList.getProblemOnahs(this.KavuahList, this.Settings);
+            probs = this.EntryList.getProblemOnahs(
+                this.KavuahList,
+                this.Settings
+            );
         }
         this.ProblemOnahs = probs;
     }
@@ -69,6 +133,20 @@ export default class AppData {
     static async getAppData() {
         if (!global.GlobalAppData) {
             global.GlobalAppData = await AppData.fromDatabase();
+
+            firstTime(GLOBAL_FIRST_TIME_RANDOM).catch(function() {
+                const {Settings, EntryList, KavuahList, UserOccasions, TaharaEvents} = global.GlobalAppData;
+                //If the location is Jerusalem and all other lists are empty, we assume this is really the first time the app was opened.
+                if (Settings.location.locationId === 28 &&
+                    !EntryList.list.length &&
+                    !UserOccasions.length &&
+                    !KavuahList.length &&
+                    !TaharaEvents.length) {
+                    //We want to have Lakewood as the default location - not Jerusalem
+                    Settings.location = Location.getLakewood();
+                    DataUtils.SetCurrentLocationOnDatabase(Settings.location);
+                }
+            });
         }
         return global.GlobalAppData;
     }
@@ -84,16 +162,13 @@ export default class AppData {
                 if (!remove) {
                     if (item instanceof Entry) {
                         appData.EntryList.add(item);
-                    }
-                    else if (item instanceof Kavuah) {
+                    } else if (item instanceof Kavuah) {
                         appData.KavuahList.push(item);
                     }
-                }
-                else {
+                } else {
                     if (item instanceof Entry) {
                         appData.EntryList.remove(item);
-                    }
-                    else if (item instanceof Kavuah) {
+                    } else if (item instanceof Kavuah) {
                         let index = appData.KavuahList.indexOf(item);
                         if (index > -1) {
                             appData.KavuahList.splice(index, 1);
@@ -120,50 +195,56 @@ export default class AppData {
             //Get the new fields for this table.
             const newFields = addedFields.filter(af => af.table === tbl);
             //Add any new fields that were added after the last update.
-            DataUtils.GetTableFields(tbl)
-                .then(fields => {
-                    for (let nf of newFields) {
-                        if (!fields.some(f => f.name === nf.name)) {
-                            DataUtils.AddTableField(nf);
-                        }
+            DataUtils.GetTableFields(tbl).then(fields => {
+                for (let nf of newFields) {
+                    if (!fields.some(f => f.name === nf.name)) {
+                        DataUtils.AddTableField(nf);
                     }
-                });
+                }
+            });
         }
     }
     /**
      * Returns an appData instance containing all the user data from the local database file.
      */
     static async fromDatabase() {
-        let settings, occasions, entryList, kavuahList, problemOnahs, taharaEvents;
-        settings = await DataUtils.SettingsFromDatabase()
-            .catch(err => {
-                warn('Error running SettingsFromDatabase.');
-                error(err);
-            });
-        occasions = await DataUtils.GetAllUserOccasions()
-            .catch(err => {
-                warn('Error running GetAllUserOccasions.');
-                error(err);
-            });
-        entryList = await DataUtils.EntryListFromDatabase()
-            .catch(err => {
-                warn('Error running EntryListFromDatabase.');
-                error(err);
-            });
-        kavuahList = await DataUtils.GetAllKavuahs(entryList)
-            .catch(err => {
-                warn('Error running GetAllKavuahs.');
-                error(err);
-            });
-        taharaEvents = await DataUtils.GetAllTaharaEvents()
-            .catch(err => {
-                warn('Error running GetAllTaharaEvents.');
-                error(err);
-            });
+        let settings,
+            occasions,
+            entryList,
+            kavuahList,
+            problemOnahs,
+            taharaEvents;
+        settings = await DataUtils.SettingsFromDatabase().catch(err => {
+            warn('Error running SettingsFromDatabase.');
+            error(err);
+        });
+        occasions = await DataUtils.GetAllUserOccasions().catch(err => {
+            warn('Error running GetAllUserOccasions.');
+            error(err);
+        });
+        entryList = await DataUtils.EntryListFromDatabase().catch(err => {
+            warn('Error running EntryListFromDatabase.');
+            error(err);
+        });
+        kavuahList = await DataUtils.GetAllKavuahs(entryList).catch(err => {
+            warn('Error running GetAllKavuahs.');
+            error(err);
+        });
+        taharaEvents = await DataUtils.GetAllTaharaEvents().catch(err => {
+            warn('Error running GetAllTaharaEvents.');
+            error(err);
+        });
 
         //After getting all the data, the problem onahs are set.
         problemOnahs = entryList.getProblemOnahs(kavuahList, settings);
 
-        return new AppData(settings, occasions, entryList, kavuahList, problemOnahs, taharaEvents);
+        return new AppData(
+            settings,
+            occasions,
+            entryList,
+            kavuahList,
+            problemOnahs,
+            taharaEvents
+        );
     }
 }
